a8fe468ddeca35579bb88bcbabe65b09bd423506
micha-aucoin <maucoin94@gmail.com>
2025-01-12 15:15:00 -0600

    view backups

---
+++ scripts/view_backups.sh
@@ -0,0 +1,13 @@
+(
+printf '%s\t%s\t%s\t%s\t%s\n' 'Namespace' 'BackupName' 'PVName' 'PVCName'
+kubectl get backups.longhorn.io -A -o=json | jq -r '
+    .items[] |
+    [
+        .metadata.namespace,
+        .metadata.name,
+        (.spec.labels.KubernetesStatus | fromjson.pvName),
+        (.spec.labels.KubernetesStatus | fromjson.pvcName)
+    ] | @tsv
+    '
+) | column -t
+
